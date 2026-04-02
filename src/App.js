f53b0af61993a88740c1b300b739e68468250d95
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-04-02 11:28:55 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom/client'
+import './index.css'
+import Counter from './Components/Counter'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+const App=()=>{
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <div className="flex-grow">
+                <Counter />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
-import './index.css'
-import Counter from './Components/Counter'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
+import App from './App'
 const a = ReactDom.createRoot(document.getElementById('root'))
 
 a.render(
-  <div className="min-h-screen flex flex-col">
-    <Header />
-    <div className="flex-grow">
-      <Counter />
-    </div>
-    <Footer />
-  </div>
+  <App></App>
 )
